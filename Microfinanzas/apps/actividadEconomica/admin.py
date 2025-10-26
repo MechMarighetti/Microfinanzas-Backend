b0ce4879cf2fdef97dead2507c117225b535169c
Mech Marighetti <mercedesmarighetti@gmail.com>
2025-10-26 18:47:11 -0300

    comprobante esta compli

--- Microfinanzas/apps/actividadEconomica/admin.py
+++ Microfinanzas/apps/actividadEconomica/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Servicio, Producto, Categoria
 
-# Register your models here.
+admin.register(Servicio)
+admin.register(Categoria)
+admin.register(Producto)
