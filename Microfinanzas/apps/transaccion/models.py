92204fe86f51fc217b78f258d0d468a18f7ab3cf
teosordo <teosordo98@gmail.com>
2025-11-08 16:29:41 -0300

    definicion de modelos de app transaccion

--- Microfinanzas/apps/transaccion/models.py
+++ Microfinanzas/apps/transaccion/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from ..Comprobante.models import Comprobante
+from ..emprendimiento.models import Emprendimiento
 
-# Create your models here.
+class Ingreso(models.Model):
+    fecha = models.DateField(default=date.today)
+    importe = models.DecimalField(max_digits=9, decimal_places=2)
+    descripcion = models.CharField(max_length=100)
+
+    comprobante_id = models.ForeignKey(Comprobante, on_delete=models.CASCADE, related_name='ingresos')
+    emprendimiento_id = models.ForeignKey(Emprendimiento, on_delete=models.CASCADE, related_name='ingresos')
+
+    def __str__(self):
+        return f"Fecha: {self.fecha}\nImporte: {self.importe}\nDescripción: {self.descripcion}"
+
+class Egreso(models.Model):
+    fecha = models.DateField(default=date.today)
+    importe = models.DecimalField(max_digits=9, decimal_places=2)
+    descripcion = models.CharField(max_length=100)
+
+    comprobante_id = models.ForeignKey(Comprobante, on_delete=models.CASCADE, related_name='egresos')
+    emprendimiento_id = models.ForeignKey(Emprendimiento, on_delete=models.CASCADE, related_name='egresos')
+
+    def __str__(self):
+        return f"Fecha: {self.fecha}\nImporte: {self.importe}\nDescripción: {self.descripcion}"
