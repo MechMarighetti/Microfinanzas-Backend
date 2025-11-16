c2ac6b73360ff0726115b095a844abaf06c9c4e0
romina forclas <rominagforclas@gmail.com>
2025-11-16 12:25:40 -0300

    ultimos cambios que no habia subido

--- Microfinanzas/apps/detalleIngreso/models.py
+++ Microfinanzas/apps/detalleIngreso/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class UnidadMedida(models.Model):
-    unique_id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
 
     def __str__(self):
@@ -9,19 +8,18 @@ class UnidadMedida(models.Model):
 
 
 class DetalleIngreso(models.Model):
-    unique_id = models.AutoField(primary_key=True)
     fecha = models.DateField()
-    unidad_medida = models.ForeignKey(UnidadMedida, on_delete=models.CASCADE)
+    unidad_medida = models.ForeignKey(UnidadMedida, on_delete=models.CASCADE, related_name='detalles_ingreso')
     cantidad = models.DecimalField(max_digits=10, decimal_places=2)
     monto = models.DecimalField(max_digits=12, decimal_places=2)
     
     
-    ingreso = models.ForeignKey('Ingreso', on_delete=models.CASCADE, null=True, blank=True)
-    servicio = models.ForeignKey('Servicio', on_delete=models.SET_NULL, null=True, blank=True)
-    producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True, blank=True)
+    actividadEconomica = models.ForeignKey('Ingreso', on_delete=models.CASCADE, null=True, blank=True)
+    transaccion = models.ForeignKey('Transaccion', on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return f"Detalle Ingreso {self.unique_id} - {self.fecha}"
 
 
-# Create your models here.
+
